813bf1ff7cdea0e88bf3b287c3398d4de3c136b5
EC2 Default User <ec2-user@ip-172-31-88-45.ec2.internal>
2021-04-03 20:18:03 +0000

    lab 7

---
+++ lec7.py
@@ -0,0 +1,25 @@
+'''
+lec 7
+'''
+
+i = 5
+while i >=0:
+    
+    try:
+        print(1/(i-3))
+    except:
+        pass
+    
+    
+    
+    i = i - 1
+    
+  
+# try:
+#     print(1/0)
+# except ZeroDivisionError:
+#     print('ZeroDivisionError')
+    
+# except:
+#     print('other errors')
+
